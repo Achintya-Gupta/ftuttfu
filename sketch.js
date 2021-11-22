d9366e2b1c45e7dbe0da7d0e395360da9ca5fea2
Achintya-Gupta <88548454+Achintya-Gupta@users.noreply.github.com>
2021-11-22 01:31:32 -1200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var name1="cheenti";
+console.log(name1);
+console.log("hdhdhddhfhdh");
+console.log("how are you?");
+console.log("fine");
+
+
+var name2="gupta";
+console.log(name2);
+
+name3 = name1.concat(name2);
+console.log(name3);
+
+var numbers = [34,45,445,45,46];
+console.log(numbers);
+
+numbers.push(90,54,908,89);
+console.log(numbers);
+
+numbers.pop();
+console.log(numbers);
+
+numbers.sort();
+console.log(numbers);
+
+numbers.reverse();
+console.log(numbers);
+
+console.log(numbers.length);
+
